e167dbcaaee762b16e7ce227f867fd22f80310a8
akshat.kumar <akshat.kumar@successive.tech>
2024-01-17 16:36:53 +0530

    Injecting Services of DbContext in application

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using DependencyInjection_WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add(injecting) services to the container(or application).
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// Injecting a service
+builder.Services.AddDbContext<StudentsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionString")));
 
 var app = builder.Build();
 
